938d8162f8d0638a5f517e174f6c76d459d3cc35
dave <dave@376e6dc9-c68f-4ac9-b4f7-ac5e2e571aec>
2012-04-20 00:08:22 +0000

    * Test branch - remove UNICODE support from NTLM auth, advertise only OEM   support.If this helps with some peculiar NetCache config, where Cntlm   simply doesn't authenticize, while not breaking NTLMv2 and higher schemes,   I'll keep everything in OEM strings.

--- ntlm.c
+++ ntlm.c
@@ -123,7 +123,7 @@ static void ntlm2_calc_resp(char **nthash, int *ntlen, char **lmhash, int *lmlen
 	return;
 }
 
-static void ntlm2sr_calc_rest(char **nthash, int *ntlen, char **lmhash, int *lmlen, char *passnt, char *challenge) {
+static void ntlm2sr_calc_resp(char **nthash, int *ntlen, char **lmhash, int *lmlen, char *passnt, char *challenge) {
 	char *sess, *nonce, *buf;
 
 	nonce = new(8 + 1);
@@ -176,7 +176,9 @@ char *ntlm_hash_nt_password(char *password) {
 	int len;
 
 	keys = new(21 + 1);
-	len = unicode(&u16, password);
+	//len = unicode(&u16, password);
+	len = strlen(password);
+	u16 = strdup(password);
 	md4_buffer(u16, len, keys);
 
 	memset(keys+16, 0, 5);
@@ -196,7 +198,9 @@ char *ntlm2_hash_password(char *username, char *domain, char *password) {
 	strcat(buf, username);
 	strcat(buf, domain);
 	uppercase(buf);
-	len = unicode(&tmp, buf);
+	//len = unicode(&tmp, buf);
+	len = strlen(buf);
+	tmp = strdup(buf);
 
 	passnt2 = new(16 + 1);
 	hmac_md5(passnt, 16, tmp, len, passnt2);
@@ -219,13 +223,13 @@ int ntlm_request(char **dst, struct auth_s *creds) {
 
 	if (!creds->flags) {
 		if (creds->hashntlm2)
-			flags = 0xa208b205;
+			flags = 0xa208b206;
 		else if (creds->hashnt == 2)
-			flags = 0xa208b207;
+			flags = 0xa208b206;
 		else if (creds->hashnt && creds->hashlm)
-			flags = 0xb207;
+			flags = 0xb206;
 		else if (creds->hashnt)
-			flags = 0xb205;
+			flags = 0xb206;
 		else if (creds->hashlm)
 			flags = 0xb206;
 		else {
@@ -364,7 +368,7 @@ int ntlm_response(char **dst, char *challenge, int challen, struct auth_s *creds
 	}
 
 	if (creds->hashnt == 2) {
-		ntlm2sr_calc_rest(&nthash, &ntlen, &lmhash, &lmlen, creds->passnt, challenge);
+		ntlm2sr_calc_resp(&nthash, &ntlen, &lmhash, &lmlen, creds->passnt, challenge);
 	}
 
 	if (creds->hashnt == 1) {
@@ -375,6 +379,7 @@ int ntlm_response(char **dst, char *challenge, int challen, struct auth_s *creds
 		lmlen = ntlm_calc_resp(&lmhash, creds->passlm, MEM(challenge, char, 24));
 	}
 
+	/*
 	if (creds->hashnt || creds->hashntlm2) {
 		tmp = uppercase(strdup(creds->domain));
 		dlen = unicode(&udomain, tmp);
@@ -383,7 +388,7 @@ int ntlm_response(char **dst, char *challenge, int challen, struct auth_s *creds
 		tmp = uppercase(strdup(creds->workstation));
 		hlen = unicode(&uhost, tmp);
 		free(tmp);
-	} else {
+	} else */ {
 		udomain = uppercase(strdup(creds->domain));
 		uuser = uppercase(strdup(creds->user));
 		uhost = uppercase(strdup(creds->workstation));
